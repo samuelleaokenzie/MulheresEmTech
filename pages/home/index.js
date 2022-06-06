e60d69ccffb9a12e096678242b8db1be46ad5c9e
samuelleaokenzie <samuel@kenzie.com.br>
2022-06-06 09:07:19 -0300

    Icon

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,24 +1,3 @@
-const jobsData = [
-  {
-    id: 0,
-    title: "Pessoa desenvolvedora front-end - React",
-    enterprise: "Kenzie",
-    location: "Curitiba",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Hibrido", "Presencial"],
-  },
-  {
-    id: 1,
-    title: "Pessoa desenvolvedora back-end - Node JS",
-    enterprise: "Brazilians in Tech",
-    location: "Rio de Janeiro",
-    descrition:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    modalities: ["Home Office", "Presencial"],
-  },
-];
-
 let jobsSelect = [];
 let $jobsCardsArea = document.querySelector(".jobs");
 let $jobSelectedCardsArea = document.querySelector(".jobs-selected-render");
